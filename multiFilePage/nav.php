108decf0f8e8db1f0617b9bac1021079bbe925f6
Jaan Krohhin <77333214+JaanKrohhin@users.noreply.github.com>
2021-11-23 23:01:44 +0200

    Add files via upload

---
+++ multiFilePage/nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <div class="topnav">
+        <a href="?">Kodu</a>
+        <a href="?leht=content/kontakt">Kontakt</a>
+        <a href="?leht=content/harjutused/moistatus">Tekst Funktsioonid</a>
+        <a href="?leht=content/dateFunc">Date funktsioonid</a>
+        <a href="?leht=content/test">PHP Harjutused</a>
+        <a href="?leht=content/lego">Lego</a>
+        <a href="?leht=content/Task8/puzzle">Puzzle</a>
+        <a href="?leht=content/Task7Calculator/calculators">Calculator</a>
+        <a href="?leht=pildid">Pildid</a>
+        <a href="?leht=sql/index">Andmebaas</a>
+    </div>
+</nav>
